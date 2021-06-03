3d9fd0d6f3826689362db4d999e4c4b644318962
Arthur Karganyan <arthur.karganyan@gmail.com>
2021-06-03 15:31:29 +0200

    OmdbApi : Added validation if OMDB_API_KEY is not present

--- lib/omdb_api.rb
+++ lib/omdb_api.rb
@@ -1,9 +1,14 @@
 class OmdbApi
   include HTTParty
   base_uri 'http://www.omdbapi.com'
+  attr_reader :api_key
+
+  def initialize
+    @api_key = ENV['OMDB_API_KEY'] || fail("OMDB_API_KEY not provided")
+  end
 
   def film_info(imdb_id)
-    res = self.class.get("/", {query: {i: imdb_id, apikey: ENV['OMDB_API_KEY']}})
+    res = self.class.get("/", {query: {i: imdb_id, apikey: api_key}})
     res.parsed_response
   end
 end
